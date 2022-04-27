532934f00c6e4746b32b0f8224c64c6da80380b2
Jeyson Alejandro Pereira <jpereiraf96@gmail.com>
2022-04-27 15:12:56 -0500

    correción import JPA en modelos
    se corrigen los imports del JPA en los modelos UsuarioModel y RolModel donde solo se importe lo necesario

--- src/main/java/com/crud/democrud/models/RolModel.java
+++ src/main/java/com/crud/democrud/models/RolModel.java
@@ -1,6 +1,11 @@
 package com.crud.democrud.models;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
 
 @Entity
 @Table(name = "rol")
