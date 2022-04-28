d43d0e58c19f934c05df805a21117eedaf9d2508
Jeyson Alejandro Pereira <jpereiraf96@gmail.com>
2022-04-28 01:14:22 -0500

    refactorización modelo rol
    implementación uso de lombok y reestructura de relación con modelo usuario

--- src/main/java/com/crud/democrud/models/RolModel.java
+++ src/main/java/com/crud/democrud/models/RolModel.java
@@ -1,57 +1,32 @@
 package com.crud.democrud.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Column;
-import javax.persistence.ManyToMany;
-import javax.persistence.CascadeType;
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
 @Entity
-@Table(name = "rol")
 public class RolModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true, nullable = false)
     private Long id;
+
+    @Column(name = "nombre")
     private String nombre;
 
-    @ManyToMany(targetEntity = UsuarioModel.class, mappedBy = "roles", cascade = CascadeType.ALL)
-    private List<UsuarioModel> usuarios;
-
-    public RolModel(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public RolModel() {
-
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public List<UsuarioModel> getUsuarios() {
-        return usuarios;
-    }
-
-    public void setUsuarios(List<UsuarioModel> usuarios) {
-        this.usuarios = usuarios;
-    }
-
+    @ManyToOne(
+            targetEntity = UsuarioModel.class,
+            fetch = FetchType.LAZY,
+            optional = false
+    )
+    @JoinColumn(name = "id_usuario")
+    @JsonBackReference
+    private UsuarioModel usuario;
 }
