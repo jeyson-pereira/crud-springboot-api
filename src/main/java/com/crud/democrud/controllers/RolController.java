bb77f294d6ded7e3dc67f939bbe1471b41bd42f0
Jeyson Alejandro Pereira <jpereiraf96@gmail.com>
2022-04-28 02:34:04 -0500

    refactorización controlador rol, uso httpStatus y ResponseEntity
    se implemento respuestas mediante ResponseEntity y estatuscode http, se hizo uso de nuevas interfaces y servicios re estructurados

--- src/main/java/com/crud/democrud/controllers/RolController.java
+++ src/main/java/com/crud/democrud/controllers/RolController.java
@@ -1,42 +1,77 @@
 package com.crud.democrud.controllers;
 
 import com.crud.democrud.models.RolModel;
-import com.crud.democrud.services.RolService;
+import com.crud.democrud.services.RolServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Optional;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/rol")
 public class RolController {
     @Autowired
-    RolService rolService;
+    private RolServiceImpl rolServiceImpl;
 
+    /**
+     * GET: Obtiene lista de roles
+     */
     @GetMapping()
-    public ArrayList<RolModel> obtenerRoles() {
-        return rolService.obtenerRoles();
-    }
-
-    @PostMapping()
-    public RolModel guardarRol(@RequestBody RolModel rol) {
-        return this.rolService.guardarRol(rol);
-    }
-
-    @GetMapping(path = "/{id}")
-    public Optional<RolModel> obtenerUsuarioPorId(@PathVariable("id") Long id) {
-        return this.rolService.obtenerPorId(id);
-    }
-
-    @DeleteMapping(path = "/{id}")
-    public String eliminarPorId(@PathVariable("id") Long id) {
-        boolean ok = this.rolService.eliminarRol(id);
-        if (ok) {
-            return "Se eliminó el rol con id " + id;
-        } else {
-            return "No pudo eliminar el rol con id" + id;
+    public ResponseEntity<ArrayList<RolModel>> obtenerRoles(){
+        ArrayList<RolModel> obtenerRoles = rolServiceImpl.obtenerRoles();
+        try {
+            return new ResponseEntity<>(obtenerRoles, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    /**
+     * POST: Guarda un nuevo rol
+     */
+    @PostMapping()
+    public ResponseEntity<RolModel> guardarNuevoRol(@RequestBody RolModel rol){
+        return  new ResponseEntity<>(rolServiceImpl.guardarRol(rol),HttpStatus.CREATED);
+    }
+
+    /**
+     * GET: Obtiene rol por buscado por id
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<?> obtenerRolPorId(@PathVariable("id") Long id){
+        Optional<RolModel> obtenerRol = rolServiceImpl.obtenerPorId(id);
+        if(obtenerRol.isPresent()){
+            return new ResponseEntity<>(obtenerRol, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("No se encontro el rol con id "+ id, HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * PUT: Actualiza rol
+     */
+    @PutMapping("/actualizar")
+    public ResponseEntity<?> actualizarRol(@RequestBody RolModel rol){
+        Optional<RolModel> rolRequerido = rolServiceImpl.obtenerPorId(rol.getId());
+        if(rolRequerido.isPresent()){
+            return new ResponseEntity<>(rolServiceImpl.actualizarRol(rol), HttpStatus.OK);
+        }
+        return new ResponseEntity<>("El usuario que deseas actualizar no existe", HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * DELETE: Elimina rol encontrado por id
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminarUnRol(@PathVariable("id") Long id){
+        Optional<RolModel> obtenerUsuario = rolServiceImpl.obtenerPorId(id);
+        if(obtenerUsuario.isPresent()){
+            rolServiceImpl.eliminarRol(id);
+            return new ResponseEntity<>("Rol eliminado exitosamente", HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity<>("El rol que deseas eliminar no existe", HttpStatus.NOT_FOUND);
+    }
+
 }
